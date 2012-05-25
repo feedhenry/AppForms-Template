d3a076eb4655fde4d061654d8dd41acf7ecf43b0
Jason Madigan <jason.madigan@feedhenry.com>
2012-05-25 12:29:38 +0100

    Initial import

---
+++ client/default/js/wufoo.js
@@ -0,0 +1,155 @@
+var WufooController = {
+  config: null,
+  all_forms: null,
+
+  init: function(config) {
+    this.config = config;
+
+    if (wufoo_config.app_type == 'single_form') {
+      // Single form app
+      this.getForm();
+    } else {
+      // Multi-form app
+      this.getFormList(true);
+    }
+  },
+
+  initWufoo: function() {
+    var self = this;
+    var interval = setInterval(function() {
+      if (typeof init !== 'undefined') {
+        // Wufoo Global - init() - available, 
+        // call and clear the interval
+        init();
+        clearInterval(interval);
+
+        // Bind events
+        self.bind();
+      }
+    }, 50);
+  },
+
+  bind: function() {
+    var self = this;
+    jQuery('input[type=submit]:visible, button[type=submit]:visible').unbind().click(function() {
+      self.submitForm();
+      return false;
+    });
+  },
+
+  submitForm: function() {
+    var self = this;
+    $fh.act({
+      "act": "submitForm",
+      "req": {
+        "form_data": jQuery('form').serialize(),
+        "form_submission_url": jQuery('form').attr('action')
+      }
+    }, function(res) {
+      self.renderFormHtml(res.html);
+      self.initWufoo();
+    }, function(msg, err) {
+      console.log('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
+    });
+  },
+
+  renderFormHtml: function(html, show_back_button) {
+    var self = this;
+    this.hideFormList();
+    this.showContentArea();
+    jQuery('#fh_wufoo_content').html(html);
+    this.initWufoo();
+    if (show_back_button) {
+      // Inject a back button
+      var back_button = jQuery('<a>').attr('href', '#').text('Back To Form List').addClass('fh_wufoo_formlist_btn').click(function() {
+        self.getFormList(false);
+        jQuery(this).remove();
+        return false;
+      });
+      jQuery('body').prepend(back_button);
+    }
+  },
+
+  getForm: function(form_hash, show_back_button) {
+    var form_hash = form_hash || wufoo_config.form_hash;
+    var self = this;
+    $fh.act({
+      "act": "getForm",
+      "req": {
+        "form_hash": form_hash
+      }
+    }, function(res) {
+      self.renderFormHtml(res.html, show_back_button);
+      self.initWufoo();
+    }, function(msg, err) {
+      console.log('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
+    });
+  },
+
+  getFormList: function(load) {
+    var self = this;
+    if (load) {
+      $fh.act({
+        act: 'getForms',
+      }, function(res) {
+        if (res) {
+          self.all_forms = res.data.Forms;
+          self.renderFormList(self.all_forms);
+        }
+      }, function(err) {
+        console.log('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
+      });
+    } else {
+      // Show existing form list
+      self.showFormList();
+      self.hideContentArea();
+    }
+  },
+
+  renderFormList: function(forms) {
+    var self = this;
+    self.showFormList();
+    self.hideContentArea();
+
+    var form_list = $('#fh_wufoo_form_list');
+    // Render buttons for each form
+    for (var i = 0; i < forms.length; i++) {
+      var form_item = forms[i];
+      var list_item = jQuery('<li>').addClass('fh_wufoo_form_li');
+      var item_button = jQuery('<button>').addClass('fh_wufoo_form').text(form_item.Name).data('form', form_item).click(function() {
+        var form_data = jQuery(this).data('form');
+        self.getForm(form_data.Hash, true);
+      });
+      list_item.append(item_button);
+      form_list.append(list_item);
+    }
+  },
+
+  hideFormList: function() {
+    jQuery('#fh_wufoo_form_list').hide();
+  },
+
+  hideContentArea: function() {
+    jQuery('#fh_wufoo_content').hide();
+  },
+
+  showFormList: function() {
+    jQuery('#fh_wufoo_form_list').show();
+    window.scrollTo(0,0);
+  },
+
+  showContentArea: function() {
+    jQuery('#fh_wufoo_content').show();
+  }
+};
+
+
+var wufoo_controller = WufooController;
+
+$fh.ready(function() {
+  if (typeof wufoo_config !== 'undefined') {
+    wufoo_controller.init(wufoo_config);
+  } else {
+    console.log('No Wufoo config available, aborting.');
+  }
+});
---
+++ cloud/main.js
@@ -0,0 +1,129 @@
+var util = require('util');
+var request = require('request');
+var jsdom = require("jsdom");
+
+/* 
+ * Here we rewrite some Wufoo paths to JavaScript and CSS, since they're relative paths
+ * rather than absolute ones. We also remove a Wufoo script tag (after form submission)
+ * from the HTML, as this JavaScript will already be loaded client side as this point.
+ */
+updateWufooHTML = function(html, remove_script, cb) {
+  var jsdom = require("jsdom"),
+      window = jsdom.jsdom().createWindow();
+
+  jsdom.jQueryify(window, './jquery.js', function() {
+    var $ = window.$;
+
+    $('html')[0].innerHTML = html;
+
+    // Process Wufoo Scripts
+    var scripts = $('script');
+    $.each(scripts, function(i, script) {
+      var script = $(script);
+      var script_source = script.attr('src');
+
+      if (script_source) {
+        script.attr('src', "https://wufoo.com" + script_source);
+      }
+
+      if (remove_script) {
+        script.remove();
+      }
+    });
+
+    var processed_html = $('html').html();
+    processed_html = processed_html.replace('/images/icons/', 'https://wufoo.com/images/icons/');
+    processed_html = processed_html.replace('/stylesheets/public/forms/', 'https://wufoo.com/stylesheets/public/forms/');
+    window.close();
+    return cb(processed_html);
+  });
+};
+
+processFormData = function(form_data, cb) {
+  var data = form_data;
+
+  // clickOrEnter needs to be set to blank or 
+  // multi-page forms won't work correctly
+  data = data.replace('clickOrEnter=click', 'clickOrEnter=');
+  return data;
+};
+
+/* 
+ * Here we get a Wufoo form's HTML, process it, and send it back to the client
+ */
+exports.getForm = function(params, callback) {
+  var wufoo_config = require('wufoo_config.js');
+  if (typeof wufoo_config == 'undefined') {
+    return callback(null, {
+      "html": "",
+      "error": "No config."
+    });
+  }
+
+  var domain = wufoo_config.wufoo_config.api_domain;
+  var form_hash = params.form_hash;
+  var url = "https://" + domain + "/forms/" + form_hash + "/";
+
+  request(url, function(error, res, body) {
+    updateWufooHTML(body, false, function(processed_html) {
+      console.log('html processing done, sending response.');
+      return callback(null, {
+        "html": processed_html
+      });
+    });
+  });
+};
+
+/* 
+ * Here we get a list of available Wufoo forms
+ */
+exports.getForms = function(params, callback) {
+  var wufoo_config = require('wufoo_config.js');
+  if (typeof wufoo_config == 'undefined') {
+    return callback(null, {
+      "error": "No config."
+    });
+  }
+
+  var domain = wufoo_config.wufoo_config.api_domain;
+  var api_key = wufoo_config.wufoo_config.api_key;
+  var forms_url = "https://" + domain + "/api/v3/forms.json";
+
+  var auth = 'Basic ' + new Buffer(api_key + ':' + 'foostatic').toString('base64');
+  var auth_header = {'Authorization': auth};
+
+  request.get({url: forms_url, headers: auth_header}, function(error, res, body) {
+    console.log('got all forms');
+    return callback(null, {
+      data: JSON.parse(body)
+    });
+  });
+};
+
+/* 
+ * Here we get submit a form to Wufoo, and return its
+ * proxied response back to the client
+ */
+exports.submitForm = function(params, callback) {
+  console.log('in submitForm with ts:' + Date.now());
+  console.log('post_data: ' + params.form_data);
+
+  var post_data = processFormData(params.form_data);
+
+  var req = request.post({
+    url: params.form_submission_url,
+    body: post_data,
+    followAllRedirects: true,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_data.length
+    }
+  }, function(error, res, body) {
+    updateWufooHTML(body, true, function(processed_html) {
+      console.log('html processing done, sending response.')
+      return callback(null, {
+        "html": processed_html
+      });
+    });
+  });
+};
