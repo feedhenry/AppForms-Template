03b94c7ca1cf311d7b781d5edaa00995594f1643
evanshortiss <evanshortiss@gmail.com>
2012-06-01 16:37:40 +0100

    Fix binding errors

--- client/default/js/fhApis.js
+++ client/default/js/fhApis.js
@@ -17,7 +17,7 @@ var apiController = {
 		for (var i = 0; i < neededApis.length; i++) {
 			var classes = neededApis[i].className;
 			for (var j = 0; j < this.bindings.length; j++) {
-				if (classes.indexOf(this.bindings[j])!=-1); {
+				if (classes.indexOf(this.bindings[j])!==-1); {
 					var element = neededApis[i].getElementsByTagName('input')[0];
 					debugger;
 					var fn = this.bindFunction(this.bindings[j]);
@@ -44,7 +44,7 @@ var apiController = {
 
 	// Returns Lat and Long as sting
 	fhCam: function(id) {
-		var field = jQuery('#' + id);
+		var field = jQuery('#' + id)[0];
 		$fh.cam({
 			act: "picture",
 			source: "photo",
@@ -61,7 +61,7 @@ var apiController = {
 
 	// Returns Lat and Long as sting
 	fhGeo: function(id) {
-		var field = jQuery('#' + id);
+		var field = jQuery('#' + id)[0];
 		debugger;
 		$fh.geoip(function(res) {
 			var str = '';
