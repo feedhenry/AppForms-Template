eb5b12bb4846f98a5b701055808ab3bd0ec50a25
evanshortiss <evanshortiss@gmail.com>
2012-06-05 10:35:35 +0100

    chaneg strcuture

--- client/default/js/wufoo.js
+++ client/default/js/wufoo.js
@@ -145,3 +145,111 @@ $fh.ready(function() {
     console.log('No Wufoo config available, aborting.');
   }
 });
+
+
+/**************** API Binding Code ******************/
+/*
+ * File that holds bindings for APIs
+ */
+
+
+var config = {
+  fields: [],
+};
+
+
+
+var apiController = {
+  bindings: ['fhgeo', 'fhcam'],
+
+  addApiCalls: function() {
+    var neededApis = document.body.getElementsByClassName('$fh');
+    for (var i = 0; i < neededApis.length; i++) {
+      var classes = neededApis[i].className;
+      for (var j = 0; j < this.bindings.length; j++) {
+        if (classes.indexOf(this.bindings[j]) !== -1) {
+          var element = neededApis[i].getElementsByTagName('input')[0];
+          jQuery('#'+element.id).unbind('click');
+          this.bindFunction(this.bindings[j], element.id);
+          j = this.bindings.length;
+        }
+      }
+    }
+  },
+
+  bindFunction: function(className, id) {
+    var fn = '';
+    var bindFn = function(id, fnName) {
+        jQuery('#'+id).bind('click', function() {
+          apiController[fnName](id);
+        });
+      };
+    switch (className) {
+      case 'fhgeo':
+        fn = 'fhGeo';
+        break;
+      case 'fhcam':
+        fn = 'fhCam';
+        break;
+    }
+    bindFn(id, fn);
+  },
+
+  // Returns Lat and Long as sting
+  fhCam: function(id) {
+    console.log('begin cam');
+    var field = jQuery('#' + id)[0];
+    var source = '';
+    console.log('got field and source');
+    navigator.notification.confirm(
+      'Would you like to take a picture or choose from Gallery?', 
+      function(btn){
+        alert(btn);
+        if(btn==2){
+          source = 'photo';
+        }
+        if(btn==1){
+          source = 'camera';
+        }
+        alert('finished btn');
+      }, 
+      'Choose Source', 
+      "Camera,Gallery,Cancel");
+    console.log('picked source cam');
+    if(source===''){
+      console.log('cancel cam');
+      return;
+    }
+    console.log('before cam');
+    $fh.cam({
+      act: "picture",
+      source: 'camera',
+      uri: true
+    }, function(res) {
+      if (res.uri) {
+        var filePath = res.uri;
+        field.value = filePath.toString();
+        ield.blur();
+        field.disabled = 'true';
+      }
+    }, function(msg, err) {
+      field.value = 'No image could be loaded/taken';
+    });
+  },
+
+  // Returns Lat and Long as sting
+  fhGeo: function(id) {
+    var field = jQuery('#' + id)[0];
+    $fh.geoip(function(res) {
+      var str = '';
+      str += 'Longitude: ' + res.longitude + ', ';
+      str += 'Latitude: ' + res.latitude;
+      console.log(str);
+      field.value = str;
+      field.blur();
+      field.disabled = 'true';
+    }, function(msg, err) {
+      field.value = 'Location could not be determined';
+    });
+  },
+}
