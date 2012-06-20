4d94051a093826cadadc48d9d70e745ff0702a95
Wei Li <wei.li@feedhenry.com>
2012-06-20 13:21:28 +0100

    add function to check online/offline state

--- client/default/js/utils/utils.js
+++ client/default/js/utils/utils.js
@@ -10,6 +10,42 @@ var UtilsFunctions = {
             })
           }
       });
+  },
+
+  isOnline: function(callback){
+    var online = true;
+    //first, check if navigator.online is available
+    if(typeof navigator.onLine != "undefined"){
+      online = navigator.onLine;
+    }
+    if(online){
+      //use phonegap to determin if the network is available
+      if(typeof navigator.network != "undefined" && typeof navigator.network.connection != "undefined"){
+        var networkType = navigator.network.connection.type;
+        if(networkType == navigator.network.connection.none){
+          online = false;
+        }
+        callback(online);
+      } else {
+        //no phonegap available, send ajax call to google to see if a connection is available
+        $.ajax({
+          url: 'http://www.google.com',
+          async: true,
+          cache: false,
+          timeout: 1000,
+          complete: function(xhr, status){
+            if(status != "success" && status != "notmodified"){
+              //have trouble connecting to google, treat it as a network issue
+              online = false;
+            }
+            callback(online);
+          }
+        })
+
+      } 
+    } else {
+      callback(online);
+    }
   }
 }
 
