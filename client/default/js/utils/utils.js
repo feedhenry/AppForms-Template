0c5a9f05caea9dd2124857ae2cb607992835587a
Jason Madigan <jason.madigan@feedhenry.com>
2012-06-22 16:18:36 +0100

    Added FeedHenry extensions for Camera, Geo, Mapping and Signature capture support

---
+++ client/default/js/utils/utils.js
@@ -0,0 +1,42 @@
+var UtilsFunctions = {
+  getLocation: function(callback){
+    $fh.geo({act:'register', interval: 0}, function(location){
+        callback(location);
+      }, function(err){
+        if(typeof navigator.geolocation != "undefined"){
+            navigator.geolocation.getCurrentPosition(function(position){
+              var location = {lon: position.coords.longitude, lat: position.coords.latitude};
+              callback(location);
+            })
+          }
+      });
+  },
+
+  isOnline: function(callback){
+    var online = true;
+    //first, check if navigator.online is available
+    if(typeof navigator.onLine != "undefined"){
+      online = navigator.onLine;
+    }
+    if(online){
+      //use phonegap to determin if the network is available
+      if(typeof navigator.network != "undefined" && typeof navigator.network.connection != "undefined"){
+        var networkType = navigator.network.connection.type;
+        if(networkType == navigator.network.connection.none){
+          online = false;
+        }
+      }
+    }
+    callback(online);
+  },
+
+  isValid: function(value){
+    var valid = false;
+    if(value && typeof value != "undefined" && value != ""){
+      valid = true;
+    }
+    return valid;
+  }
+}
+
+var utils = UtilsFunctions;
