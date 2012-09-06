11690698e4297247ebc4b378754f88e608da3e9d
Eoin Crosbie <eoin.crosbie@feedhenry.com>
2012-09-06 17:03:58 +0100

    Fix geo location to return either lat/lon or easting/northing

--- cloud/inline.js
+++ cloud/inline.js
@@ -44,7 +44,7 @@ exports = module.exports = function(opts, cb) {
       });
     }
     // API binding names
-    var bindings = ['fhgeo', 'fhcam', 'fhdate', 'fhtime'];
+    var bindings = ['fhgeo', 'fhcam', 'fhdate', 'fhtime', 'fhpics'];
     // Remove link to wufoo
     $('#logo a').attr('href', '#');
     // Do field logic function
