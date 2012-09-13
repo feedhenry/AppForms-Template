f35ffa5862486283d7e7eb9c7056b94ab9106532
psdev.dev@example.com <psdev.dev@example.com>
2012-09-13 09:52:24 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -146,7 +146,7 @@ getConfig = function(callback) {
  * Here we get a Wufoo form's HTML, process it, and send it back to the client
  */
 exports.getForm = function(params, callback) {
-  console.log(new Date() + ' :: exports.getForm');
+  console.log(new Date() + ' :: exports.getForm - START');
   getConfig(function(err, wufoo_config) {
     //console.log('after getConfig');
     if (err != null) {
@@ -208,7 +208,7 @@ exports.getForm = function(params, callback) {
                 }
               }, function(error, res, body) {
                 updateWufooHTML(form_hash, updated, body, false, function(processed_html) {
-                  console.log(new Date() + ' :: updateWufooHTML 1');
+                  console.log(new Date() + ' :: exports.getForm - updateWufooHTML 1');
                   return callback(null, {
                     "html": processed_html
                   });
@@ -217,7 +217,7 @@ exports.getForm = function(params, callback) {
             } else {
               request(url, function(error, res, body) {
                 updateWufooHTML(form_hash, updated, body, false, function(processed_html) {
-                  console.log(new Date() + ' :: updateWufooHTML 2');
+                  console.log(new Date() + ' :: exports.getForm - updateWufooHTML 2');
                   return callback(null, {
                     "html": processed_html
                   });
@@ -251,7 +251,7 @@ exports.getForm = function(params, callback) {
             }
           }, function(error, res, body) {
             updateWufooHTML(form_hash, updated, body, false, function(processed_html) {
-              console.log(new Date() + ' :: updateWufooHTML 3');
+              console.log(new Date() + ' :: exports.getForm - updateWufooHTML 3');
               return callback(null, {
                 "html": processed_html
               });
@@ -260,7 +260,7 @@ exports.getForm = function(params, callback) {
         } else {
           request(url, function(error, res, body) {
             updateWufooHTML(form_hash, updated, body, false, function(processed_html) {
-              console.log(new Date() + ' :: updateWufooHTML 4');
+              console.log(new Date() + ' :: exports.getForm - updateWufooHTML 4');
               return callback(null, {
                 "html": processed_html
               });
@@ -278,7 +278,7 @@ exports.getForm = function(params, callback) {
  * Here we get a list of available Wufoo forms
  */
 exports.getForms = function(params, callback) {
-  console.log(new Date() + ' :: exports.getForms');
+  console.log(new Date() + ' :: exports.getForms - START');
   getConfig(function(err, wufoo_config) {
     if (err != null) {
       return callback(null, err);
@@ -308,7 +308,7 @@ exports.getForms = function(params, callback) {
         url: forms_url,
         headers: auth_header
       }, function(error, res, body) {
-        console.log(new Date() + ' :: request.get');
+        console.log(new Date() + ' ::  exports.getForms - request.get');
         return callback(null, {
           data: JSON.parse(body)
         });
@@ -322,6 +322,7 @@ exports.getForms = function(params, callback) {
  * proxied response back to the client
  */
 exports.submitForm = function(params, callback) {
+  console.log(new Date() + ' :: exports.submitForm - START');
   var multipart_data = formDataToMultipart(params.form_data);
   var req = request({
     method: 'POST',
@@ -332,7 +333,9 @@ exports.submitForm = function(params, callback) {
     },
     multipart: multipart_data
   }, function(e, r, b) {
+    console.log(new Date() + ' :: exports.submitForm - FINISHED SENDING DATA');
     updateWufooHTML(params.form_hash, null, b, true, function(processed_html) {
+      console.log(new Date() + ' :: exports.submitForm - RETURN 1');
       return callback(null, {
         "html": processed_html + '<button onclick="WufooController.showHome()">Back to Forms</button>'
       });
