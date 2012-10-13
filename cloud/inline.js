26b1f197b9b62215ef2f21ab85dcaa3f23276306
David Martin <david.martin@feedhenry.com>
2012-10-13 01:27:48 +0100

    Let client handle showing/hiding remove image button

--- cloud/inline.js
+++ cloud/inline.js
@@ -62,7 +62,6 @@ exports = module.exports = function(opts, cb) {
         var imgThumb = $('<img>');
         var removeThumb = $('<button>');
         removeThumb.attr('class', 'apibtn removeThumb');
-        removeThumb.attr('style', 'display:none')
         removeThumb.html('Remove Image');
         imgThumb.attr('class', 'imageThumb');
         imgField.append(imgFieldDesc);
