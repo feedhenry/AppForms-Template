653c1fc69f4574df3438fe4781582656f2417067
Jason Madigan <jason.madigan@feedhenry.com>
2012-10-24 20:39:33 +0100

    Form list rendering

--- client/default/js/views/form_list.js
+++ client/default/js/views/form_list.js
@@ -7,47 +7,46 @@ $fh.ready(function() {
       'click button#add': 'addItem'
     },
 
+    templates: {
+      header: ['<h2>Your Forms</h2>', '<h4>Choose a form from the list below</h4>']  
+    },
+
     initialize: function() {
-      _.bindAll(this, 'render', 'addItem', 'appendItem');
+      _.bindAll(this, 'render', 'appendForm');
 
       this.collection = App.forms_collection;
-      this.collection.bind('add', this.appendItem);
+      this.collection.bind('add', this.appendForm);
       this.collection.bind("remove", this.render, this);
 
       App.forms_collection.fetch();
 
-      this.counter = 0;
       this.render();
     },
 
     render: function() {
+      var self = this;
       console.log('render FormListView');
 
       this.header = new HeaderView();
 
-      var self = this;
+      // Empty our existing view
       $(this.el).empty();
-      $(this.el).append("<button id='add'>Add list item</button>");
+
+      // Add header
+      $(this.el).append(this.templates.header.join(''));
+
       $(this.el).append("<ul></ul>");
-      _(this.collection.models).each(function(item) {
-        self.appendItem(item);
+      _(this.collection.models).each(function(form) {
+        self.appendForm(form);
       }, this);
       $(this.el).show();
     },
 
-    addItem: function() {
-      this.counter++;
-      var item = new FormModel();
-      item.set({
-        part2: item.get('part2') + this.counter
-      });
-      this.collection.add(item);
-    },
-
-    appendItem: function(item) {
-      console.log('appendItem called!')
-
-      $('ul', this.el).append("<li>" + item.get('part1') + " " + item.get('part2') + "</li>");
+    appendForm: function(form) {
+      console.log('appendForm called!')
+      // Todo: Move into new view
+      var html = _.template('<li><button><%= name %></button></li>', { name: form.get("Name") });
+      $('ul', this.el).append(html);
     }
   });
 
