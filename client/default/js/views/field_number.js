527ebb6519a35309c94840512777a933db97bc0f
David Martin <david.martin@feedhenry.com>
2012-10-25 15:46:28 +0100

    Abstract common field elements to field view

--- client/default/js/views/field_number.js
+++ client/default/js/views/field_number.js
@@ -1,20 +1,3 @@
-FieldNumberView = Backbone.View.extend({
-
-  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="number">'],
-
-  initialize: function() {
-    this.render();
-  },
-
-  render: function() {
-    this.$el.append(_.template(this.template.join(''), {
-      "id": this.options.field.ID,
-      "title": this.options.field.Title
-    }));
-    if (this.options.field.IsRequired === '1') {
-      this.$el.find('#' + this.options.field.ID).rules("add", {
-        "required": true
-      });
-    }
-  }
+FieldNumberView = FieldView.extend({
+  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="number">']
 });
--- client/default/js/views/field_text.js
+++ client/default/js/views/field_text.js
@@ -1,20 +1,3 @@
-FieldTextView = Backbone.View.extend({
-
-  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="text">'],
-
-  initialize: function() {
-    this.render();
-  },
-
-  render: function() {
-    this.$el.append(_.template(this.template.join(''), {
-      "id": this.options.field.ID,
-      "title": this.options.field.Title
-    }));
-    if (this.options.field.IsRequired === '1') {
-      this.$el.find('#' + this.options.field.ID).rules("add", {
-        "required": true
-      });
-    }
-  }
+FieldTextView = FieldView.extend({
+  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="text">']
 });
