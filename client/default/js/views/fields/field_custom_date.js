9d2647c1fac2e951f4cbdd0c79c6cbd019cc0b74
Jason Madigan <jason.madigan@feedhenry.com>
2012-10-30 12:03:52 +0000

    Custom date renderer

--- client/default/js/views/fields/field_custom_date.js
+++ client/default/js/views/fields/field_custom_date.js
@@ -1,3 +1,27 @@
 FieldCustomDateView = FieldView.extend({
-  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="date">']
+  extension_type: 'fhtime',
+
+  templates: {
+    input: '<label for="<%= id %>"><%= title %></label><input id="<%= id %>" name="<%= id %>" type="text">'
+  },
+
+  render: function() {
+    // construct field html
+    this.$el.append(_.template(this.templates.input, {
+      "id": this.model.get('ID'),
+      "title": this.model.get('Title')
+    }));
+
+    // Add button
+    this.addButton(this.$el, this.extension_type, 'Capture Date');
+
+    // add to dom
+    this.options.formEl.append(this.$el);
+    this.show();
+  },
+
+  action: function(el) {
+    var ds = new moment().format('DD/MM/YYYY');
+    $('input', this.$el).val(ds).blur();
+  }
 });
