ccf706e26bf62b90e926f5f96865fc29e284aee5
Jason Madigan <jason.madigan@feedhenry.com>
2012-10-30 11:58:02 +0000

    Apply fhtime to the right renderer

--- client/default/js/views/fields/field_custom_time.js
+++ client/default/js/views/fields/field_custom_time.js
@@ -1,3 +1,27 @@
 FieldCustomTimeView = FieldView.extend({
-  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="time">']
+  extension_type: 'fhtime',
+
+  templates: {
+    input: '<label for="<%= id %>"><%= title %></label><input id="<%= id %>" name="<%= id %>" type="time">'
+  },
+
+  render: function() {
+    // construct field html
+    this.$el.append(_.template(this.templates.input, {
+      "id": this.model.get('ID'),
+      "title": this.model.get('Title')
+    }));
+
+    // Add button
+    this.addButton(this.$el, this.extension_type, 'Capture Time');
+
+    // add to dom
+    this.options.formEl.append(this.$el);
+    this.show();
+  },
+
+  action: function(el) {
+    var ts = new moment().format('HH:mm:ss');
+    $('input', this.$el).val(ts).blur();
+  }
 });
