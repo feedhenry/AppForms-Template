ab355b5a7bb63906b10032d9f26c4acca23612da
Jason Madigan <jason.madigan@feedhenry.com>
2012-11-01 15:56:57 +0000

    Fixed pending model, using wrong collection/store

--- client/default/js/models/pending.js
+++ client/default/js/models/pending.js
@@ -1,8 +1,8 @@
 PendingModel = FormModel.extend({});
 
 PendingCollection = Backbone.Collection.extend({
-  model: DraftModel,
-  fhStorage: new Store("drafts", "getForms")
+  model: PendingModel,
+  fhStorage: new Store("pending", "getForms")
 });
 
 App.collections.pending = new PendingCollection();
