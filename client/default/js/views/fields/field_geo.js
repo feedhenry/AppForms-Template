50d4e221f12c4d464add1d06911e6533df5e0222
Jason Madigan <jason.madigan@feedhenry.com>
2012-11-02 10:03:34 +0000

    Implemented fhgeo

--- client/default/js/views/fields/field_geo.js
+++ client/default/js/views/fields/field_geo.js
@@ -1,3 +1,36 @@
 FieldGeoView = FieldView.extend({
-  template: ['<label for="<%= id %>"><%= title %></label>','<input id="<%= id %>" name="<%= id %>" type="text">']
+  extension_type: 'fhgeo',
+
+  templates: {
+    input: '<label for="<%= id %>"><%= title %></label><input id="<%= id %>" name="<%= id %>" type="text" disabled>'
+  },
+
+  render: function() {
+    // construct field html
+    this.$el.append(_.template(this.templates.input, {
+      "id": this.model.get('ID'),
+      "title": this.model.get('Title')
+    }));
+
+    // Add button
+    this.addButton(this.$el, this.extension_type, 'Capture Location');
+
+    // add to dom
+    this.options.parentEl.append(this.$el);
+    this.show();
+  },
+
+  action: function(el) {
+    var ds = new moment().format('YYYY-MM-DD');
+    var input = $('input', this.$el);
+
+    $fh.geo(function(res) {
+      var location = '(' + res.lat + ', ' + res.lon + ')';
+      input.val(location);
+    }, function(msg, err) {
+      input.val('Location could not be determined');
+    });
+    input.blur();
+  }
+
 });
