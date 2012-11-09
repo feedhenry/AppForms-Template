6e4117deef11b380b17761074eaf338aa8c83528
Jason Madigan <jason.madigan@feedhenry.com>
2012-11-09 11:21:17 +0000

    Changes to pending area buttons

--- client/default/js/views/pending_submitting_item.js
+++ client/default/js/views/pending_submitting_item.js
@@ -1,6 +1,6 @@
 PendingSubmittingItemView = PendingItemView.extend({
   templates: {
-    item: '<span class="name"><%= name %></span><br/><span class="ts">Saved: <%= timestamp %></span><button class="button button-positive submit-item second_button">Submit</button><span class="chevron"></span>',
+    item: '<span class="name"><%= name %></span><br/><span class="ts">Saved: <%= timestamp %></span><span class="chevron"></span>',
   },
   //Added submit button for test only, remove after
 
