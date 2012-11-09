480e7e466e0018d496c306558ee3589107dc64a0
Jason Madigan <jason.madigan@feedhenry.com>
2012-11-09 13:03:57 +0000

    removed old submit

--- client/default/js/views/draft_item.js
+++ client/default/js/views/draft_item.js
@@ -4,13 +4,6 @@ DraftItemView = PendingItemView.extend({
     item: '<span class="name"><%= name %></span><br/><span class="ts"><%= timestamp %></span><button class="button button-negative delete-item second_button">Delete</button><span class="chevron"></span>',
   },
 
-
-  submit: function() {
-     App.collections.pending_submitting.add(this.model);
-     App.collections.drafts.remove(this.model);
-    return false;
-  },
-
   show: function() {
     App.views.form = new DraftView({
       model: this.model
