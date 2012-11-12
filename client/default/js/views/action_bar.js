de23f08afb601d49b917a690df6aa6cf6f73fa82
Jason Madigan <jason.madigan@feedhenry.com>
2012-11-12 17:12:51 +0000

    Action bar height/width buttons

--- client/default/js/views/action_bar.js
+++ client/default/js/views/action_bar.js
@@ -15,30 +15,33 @@ ActionBarView = Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html('<button class="previous hidden button">Previous</button><button class="next hidden button">Next</button><button class="saveDraft hidden button button-main">Save Draft</button><button class="submit hidden button button-positive">Submit</button>');
+    this.$el.html('<button class="saveDraft hidden button button-main">Save Draft</button><button class="previous hidden button">Previous</button><button class="next hidden button">Next</button><button class="submit hidden button button-positive">Submit</button>');
     this.options.parentEl.append(this.$el);
   },
 
   activePageChange: function (model, pageIndex) {
     // show/hide previous/next/submit buttons accordingly
-    this.$el.find('button').addClass('hidden');
-    this.$el.find('.saveDraft').removeClass('hidden');
+    this.$el.find('button').addClass('hidden').removeClass('two_button, three_button');
 
     var numPages = model.pages.length;
     if (numPages < 2) {
       // single-page. show submit button
-      this.$el.find('.submit').removeClass('hidden');
+      this.$el.find('.submit').removeClass('hidden').addClass('two_button');
+      this.$el.find('.saveDraft').removeClass('hidden').addClass('two_button');
     } else if (pageIndex === 0) {
       // multi-page, first page. show next button
-      this.$el.find('.next').removeClass('hidden');
+      this.$el.find('.next').removeClass('hidden').addClass('two_button');
+      this.$el.find('.saveDraft').removeClass('hidden').addClass('two_button');
     } else if (pageIndex === (numPages - 1)) {
       // multi-page, last page. show submit button and previous
-      this.$el.find('.submit').removeClass('hidden');
-      this.$el.find('.previous').removeClass('hidden');
+      this.$el.find('.submit').removeClass('hidden').addClass('three_button');
+      this.$el.find('.previous').removeClass('hidden').addClass('three_button');
+      this.$el.find('.saveDraft').removeClass('hidden').addClass('three_button');
     } else {
       // multi-page, in-between page. show next & previous
-      this.$el.find('.next').removeClass('hidden');
-      this.$el.find('.previous').removeClass('hidden');
+      this.$el.find('.next').removeClass('hidden').addClass('three_button');
+      this.$el.find('.previous').removeClass('hidden').addClass('three_button');
+      this.$el.find('.saveDraft').removeClass('hidden').addClass('three_button');
     }
   },
 
