12eec6f64f74bdfa427390c4ff344eeba9a39e6e
Jason Madigan <jason.madigan@feedhenry.com>
2012-11-13 11:49:24 +0000

    Sent - limit to maxSize

--- client/default/js/models/sent.js
+++ client/default/js/models/sent.js
@@ -11,6 +11,23 @@ SentCollection = Backbone.Collection.extend({
   model: SentModel,
   store: new FHBackboneDataActSync("sent"),
   sync: FHBackboneDataActSyncFn,
+
+  maxSize: 5,
+
+  initialize: function() {
+    this.on('add', this.checkSize);
+  },
+
+  checkSize: function() {
+    console.log('check sent size!');
+    if (this.length > this.maxSize) {
+      var toDelete = this.models.slice(0, this.models.length - this.maxSize);
+      _(toDelete).forEach(function(model) {
+        model.destroy();
+      });
+    }
+  },
+
   create: function(attributes, options) {
     attributes.submittedAt = new Date().getTime();
     return Backbone.Collection.prototype.create.call(this, attributes, options);
