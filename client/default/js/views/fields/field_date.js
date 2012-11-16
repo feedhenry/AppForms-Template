ed39dbde986507cde3a0d0abfa3ed7e17a824855
Michael Nairn <michael.nairn@feedhenry.com>
2012-11-16 13:34:39 +0000

    Fix date field rendering and restoration.

--- client/default/js/views/fields/field_date.js
+++ client/default/js/views/fields/field_date.js
@@ -7,14 +7,14 @@ FieldDateView = FieldView.extend({
     if (value && !_.isEmpty(value)) {
       $.each(value, function(id, val) {
         if (val && !_.isEmpty(val)) {
-          $("#" + id).val(new moment(val).format('YYYY-MM-DD'));
+          $("#" + id).val(new moment(val, "YYYYMMDD").format('YYYY-MM-DD'));
         }
       });
     }
     var val = $('#' + this.model.get('ID')).val();
     value = {};
     if(val !== "") {
-      value[this.model.get('ID')] = new moment(value).format('YYYYMMDD');
+      value[this.model.get('ID')] = new moment(val).format('YYYYMMDD');
     }
     return value;
   }
