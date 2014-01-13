689db2ba0789a2f5461e47550ef80ecf40c188df
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-01-13 13:10:20 +0000

    Refactored page rendering

--- client/default/lib/appFormjs-backbone.js
+++ client/default/lib/appFormjs-backbone.js
@@ -4133,7 +4133,7 @@ var FormView = BaseView.extend({
   render: function() {
 
     // this.initWithForm(this.form, this.params);
-    self.el.find("#fh_appform_container.fh_appform_body").append(this.templates.buttons);
+    this.el.find("#fh_appform_container.fh_appform_body").append(this.templates.buttons);
     this.rebindButtons();
     this.pageViews[0].show();
     this.checkPages();
