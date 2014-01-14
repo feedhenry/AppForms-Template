fa1c2384b28bdcbdfc03944ef12fc658b90537fb
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-01-14 18:16:46 +0000

    Removed fh_form_body

--- client/default/lib/appFormjs-backbone.js
+++ client/default/lib/appFormjs-backbone.js
@@ -4156,7 +4156,7 @@ var FormView = BaseView.extend({
   render: function() {
 
     // this.initWithForm(this.form, this.params);
-    this.el.find("#fh_appform_container.fh_appform_body").append(this.templates.buttons);
+    this.el.find("#fh_appform_container.fh_appform_form").append(this.templates.buttons);
     this.rebindButtons();
     this.pageViews[0].show();
     this.checkPages();
