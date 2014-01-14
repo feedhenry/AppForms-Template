f196f711adaa7d987685f4d9db227200557928d5
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-01-14 18:39:29 +0000

    Fixed page render bug

--- client/default/lib/appFormjs-backbone.js
+++ client/default/lib/appFormjs-backbone.js
@@ -3829,7 +3829,7 @@ PageView=BaseView.extend({
 
     //Need to add the page title and description
     this.$el.append(_.template(this.templates.pageTitle, {pageTitle: this.model.getName()}));
-    this.$el.append(_.template(this.templates.pageTitle, {pageDescription: this.model.getDescription()}));
+    this.$el.append(_.template(this.templates.pageDescription, {pageDescription: this.model.getDescription()}));
 
     // add to parent before init fields so validation can work
     this.options.parentEl.append(this.$el);
