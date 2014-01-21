b8ce6eefc497b0286a62a46061cd0bcd60e4bcfd
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-01-21 12:49:43 +0000

    Refactored input

--- client/default/lib/appFormjs-backbone.js
+++ client/default/lib/appFormjs-backbone.js
@@ -1911,7 +1911,7 @@ FieldView = Backbone.View.extend({
   addInputButtonClass: ".fh_appform_addInputBtn", //TODO Need to remove hard-coded strings for these names
   removeInputButtonClass: ".fh_appform_removeInputBtn",
   fieldWrapper: '<div class="fh_appform_input_wrapper"></div>',
-  input: "<input class='fh_appform_input' data-field='<%= fieldId %>' data-index='<%= index %>' type='<%= inputType %>'/>",
+  input: "<input class='fh_appform_field_input' data-field='<%= fieldId %>' data-index='<%= index %>' type='<%= inputType %>'/>",
   inputTemplate: "<div id='wrapper_<%= fieldId %>_<%= index %>' style='width:100%'>  <div class='<%= required %> fh_appform_field_title fh_appform_field_numbering'> <%=index + 1%>.  </div> <div>  <%= inputHtml %>  </div>  <div class='fh_appform_errorMsg hidden'>  </div>  </div><br style='clear:both'/>",
 
 
