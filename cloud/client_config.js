627e103998c60ad66c6409a74d9983f4c236d426
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-01-29 16:01:26 +0000

    Changed client_config to not override user defined values

--- cloud/client_config.js
+++ cloud/client_config.js
@@ -2,7 +2,7 @@ exports.config = {
   // NOTE: If force_cloud_config_updates is set to true, then all the fields are updated and old fields are deleted.
   //       if set to a single name then this field will be updated or deleted
   //       if set to an array of names then these fields will be updated or deleted
-  "force_cloud_config_updates": true,
+  "force_cloud_config_updates": false,
   "cam_quality": 50,
   "cam_targetWidth": 2048,
   "cam_targetHeight": 1536,
@@ -11,7 +11,6 @@ exports.config = {
   "logger" : false,
   "max_retries" : 0,
   "use_chunking" : true,
-  "logger": false,
   "timeout" : 30,
   "log_line_limit": 300,
   "log_email": "logs.enterpriseplc@feedhenry.com",
