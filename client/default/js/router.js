4dfdd502f76cddd7154c667a97aa9a3d47c5eb38
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-02-10 10:47:34 +0000

    Removed local dev config

--- client/default/js/router.js
+++ client/default/js/router.js
@@ -54,8 +54,8 @@ App.Router = Backbone.Router.extend({
     $fh.ready({}, function() {
       $fh.init({}, function() {
         /**** LOCAL DEV USAGE *****/
-        $fh.cloud_props.hosts.debugCloudUrl="http://127.0.0.1:3001";
-        $fh.app_props.host="http://127.0.0.1:3001";
+//        $fh.cloud_props.hosts.debugCloudUrl="http://127.0.0.1:3001";
+//        $fh.app_props.host="http://127.0.0.1:3001";
         $fh.forms.init({}, function() {
           $fh.forms.getTheme({"fromRemote" : false, "css" : true}, function(err, themeCSS){
 
