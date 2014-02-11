0a958c44c42245dd6c731338b4a7b19c00ca68c6
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-02-11 15:41:50 +0000

    Updated package.json

--- cloud/application.js
+++ cloud/application.js
@@ -1,6 +1,6 @@
-var webapp = require('fh-webapp-test');
+var webapp = require('fh-webapp-beta');
 var express = require('express');
-$fh = require('fh-api-test');
+$fh = require('fh-api-beta');
 var mainjs = require('main.js');
 
 var app = express();
