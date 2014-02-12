92283252dab39f2dd65dfc653f1de23904965eeb
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-02-12 10:28:44 +0000

    Added wrapper

---
+++ cloud/application.js
@@ -0,0 +1,16 @@
+var webapp = require('fh-webapp-beta');
+var express = require('express');
+$fh = require('fh-api-beta');
+var mainjs = require('main.js');
+
+var app = express();
+app.use('/sys', webapp.sys(mainjs));
+app.use('/mbaas', webapp.mbaas);
+app.use('/cloud', webapp.cloud(mainjs));
+
+// You can define custom URL handlers here, like this one:
+app.use('/', function(req, res){
+  res.end('Your Cloud App is Running');
+});
+
+module.exports = app.listen(process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001);
