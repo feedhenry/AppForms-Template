07af2a35fa7f5db7584c65bede731dfba51fc24e
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-02-19 12:27:03 +0000

    Updated settings styling

--- client/default/js/views/settings.js
+++ client/default/js/views/settings.js
@@ -5,7 +5,7 @@ $(function() {
       "click #cancelBtn":"cancel",
       "click #saveBtn":"save"
     },
-    buttons:"<div style='margin: 20px 20px 20px 20px;'><button class='fh_appform_button_cancel' style='width:50%' type='button' id='cancelBtn'>Cancel</button><button class='fh_appform_button_action' style='width:50%'  type='button' id='saveBtn'>Save</button></div>",
+    buttons:"<div style='margin: 20px 20px 20px 20px;'><button class='fh_appform_button_cancel' style='width:45%;margin-right:25px;' type='button' id='cancelBtn'>Cancel</button><button class='fh_appform_button_action' style='width:45%;'  type='button' id='saveBtn'>Save</button></div>",
     render:function(){
       SettingsView.__super__.render.apply(this);
       this.$el.append(this.buttons);
