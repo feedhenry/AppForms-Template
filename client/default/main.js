a32aa789f42ba8e6efd45197afc70b5684a34457
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-02-19 09:36:54 +0000

    Updated core js-sdk

--- client/default/main.js
+++ client/default/main.js
@@ -1,4 +1,4 @@
-/*! FeedHenry-App-Forms-App-Generator - v0.3.10 - 2014-02-18
+/*! FeedHenry-App-Forms-App-Generator - v0.3.10 - 2014-02-19
 * https://github.com/feedhenry/Wufoo-Template/
 * Copyright (c) 2014 FeedHenry */
 
