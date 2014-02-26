86983fda4b6f687ec5cefa9a9ea9dc45e68c6805
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-02-26 17:10:04 +0000

    Fixed bug in rules engine

--- client/default/lib/appFormjs-core.js
+++ client/default/lib/appFormjs-core.js
@@ -5986,7 +5986,7 @@ function rulesEngine (formDef) {
 
         async.eachSeries(fieldDefinition.fieldOptions.definition.options, function(choice, cb){
           for(var choiceName in choice){
-            optionsInCheckbox.push(choiceName);
+            optionsInCheckbox.push(choice[choiceName]);
           }
           return cb();
         }, function(err){
