a4b9106a897fab7844632850d20c61b283118ac9
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-03-26 17:07:34 +0000

    Updated template app

--- client/default/main.js
+++ client/default/main.js
@@ -1,4 +1,4 @@
-/*! FeedHenry-App-Forms-App-Generator - v0.3.10 - 2014-03-21
+/*! FeedHenry-App-Forms-App-Generator - v0.3.10 - 2014-03-26
 * https://github.com/feedhenry/Wufoo-Template/
 * Copyright (c) 2014 FeedHenry */
 
