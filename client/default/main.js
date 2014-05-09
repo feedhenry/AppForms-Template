6e92f2d07ea66df83fee5fec141a523f853150ca
craig <craig.brookes@feedhenry.com>
2014-05-09 16:47:48 +0100

    latest jssdk changes

--- client/default/main.js
+++ client/default/main.js
@@ -1,4 +1,4 @@
-/*! FeedHenry-App-Forms-App-Generator - v0.3.11 - 2014-05-08
+/*! FeedHenry-App-Forms-App-Generator - v0.3.11 - 2014-05-09
 * https://github.com/feedhenry/Wufoo-Template/
 * Copyright (c) 2014 FeedHenry */
 
