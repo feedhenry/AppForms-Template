5c6036d22fe4da989e978fb02f919c7f29192c9a
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-05-20 08:58:00 +0100

    Updated online status functionality

--- client/default/js/router.js
+++ client/default/js/router.js
@@ -46,17 +46,15 @@ App.Router = Backbone.Router.extend({
 
         $fh.ready({}, function() {
 
-            document.addEventListener("online", function(){
+            $fh.forms.config.mbaasOnline(function(){
                 $fh.forms.log.d("Device online");
                 $('.fh_appform_alert_offline').hide();
-                $fh.forms.config.online();
-            }, false);
+            });
 
-            document.addEventListener("offline", function(){
+            $fh.forms.config.mbaasOffline(function(){
                 $fh.forms.log.d("Device offline");
                 $('.fh_appform_alert_offline').show();
-                $fh.forms.config.offline();
-            }, false);
+            });
 
             if (window.PhoneGap || window.cordova) {
                 document.addEventListener("deviceready", function() {
