2dc9c43dbf84b8c45fffe0da8dc163298b1f4d92
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-06-06 11:22:30 +0100

    Niall Additions

--- client/default/js/views/form.js
+++ client/default/js/views/form.js
@@ -8,36 +8,57 @@ $fh.ready({}, function() {
                 params.formId = params.form.getFormId();
             }
 
-            this.loadForm(params, function() {
-                self.submission.on("savedraft", function(submission) {
-                    App.views.header.showDrafts(true);
-                    App.views.form = null;
-                    refreshSubmissionCollections();
-                });
-                self.submission.on("submit", function() {
-                    App.views.header.showPending(true);
-                    App.views.form = null;
-                    refreshSubmissionCollections();
-                });
+            var submission = self.model.coreModel.newSubmission();
 
-                self.submission.on("progress", function(progress) {
-                    console.log("PROGRESS", progress, this);
-                });
-                self.submission.on("submitted", function() {
-                    console.log("SUBMITTED", this);
-                });
-                self.submission.on("error", function(errorMessage) {
-                    console.log("ERROR", errorMessage);
-                });
-                self.submission.on("inprogress", function(uploadTask) {
-                    console.log("READY FOR UPLOAD ", this, uploadTask);
-                });
-                self.trigger("loaded");
-                if (params.autoShow) {
-                    self.$el.show();
-                }
-                self.render();
+            selectJobId(function(err, job){
+                    //job  == json object
+
+                var fieldName = job.key;
+                var value = job.value;
+
+                findFieldIdByName("somename", self.model.coreModel, function(fieldId){
+                    submission.addInputValue({fieldId: fieldId, value: value}, function(err, res){
+                        params.submission = submission;
+                        this.loadForm(params, function() {
+                        self.submission.on("savedraft", function(submission) {
+                            App.views.header.showDrafts(true);
+                            App.views.form = null;
+                            refreshSubmissionCollections();
+                        });
+                        self.submission.on("submit", function() {
+                            App.views.header.showPending(true);
+                            App.views.form = null;
+                            refreshSubmissionCollections();
+                        });
+
+                        self.submission.on("progress", function(progress) {
+                            console.log("PROGRESS", progress, this);
+                        });
+                        self.submission.on("submitted", function() {
+                            console.log("SUBMITTED", this);
+                        });
+                        self.submission.on("error", function(errorMessage) {
+                            console.log("ERROR", errorMessage);
+                        });
+                        self.submission.on("inprogress", function(uploadTask) {
+                            console.log("READY FOR UPLOAD ", this, uploadTask);
+                        });
+                        self.trigger("loaded");
+                        if (params.autoShow) {
+                            self.$el.show();
+                        }
+
+                        self.render();
+                    });
+                        
+                        
+                    });
+                })
             });
+
+            
+
+            
         }
     });
 });
